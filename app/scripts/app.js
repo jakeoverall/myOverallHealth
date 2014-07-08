bf68a4915f78774add074a6b4f8c6b5eeee99f96
joverall22 <joverall22@gmail.com>
2014-07-08 01:26:32 -0600

    adds general layout

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+﻿var myHealth = angular.module('myHealth', []);
+
+myHealth.controller('navCtrl', ['$scope', function ($scope) {
+    $scope.nav = [{
+        'name': 'Home'
+    }, {
+        'name': 'Download'
+    }, {
+        'name': 'Demo'
+    },{
+        'name': 'About'
+    }, {
+        'name': 'Contact'
+    }];
+}]);
+
+
+
+$('#myTab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+});
+
+$(function () {
+    $('#myTab a:last').tab('show');
+})
