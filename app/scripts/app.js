435bd39ba9da684179262634f5ec3deb38827287
joverall22 <joverall22@gmail.com>
2014-07-08 03:43:28 -0600

    sets up ngRoute for page linking

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,45 @@
-﻿var myHealth = angular.module('myHealth', []);
+﻿var myHealth = angular.module('myHealth', ['ngRoute']);
 
 myHealth.controller('navCtrl', ['$scope', function ($scope) {
     $scope.nav = [{
-        'name': 'Home'
-    }, {
-        'name': 'Download'
+        'name': 'Download',
+        'link': '#/download'
     }, {
         'name': 'Demo'
-    },{
-        'name': 'About'
     }, {
-        'name': 'Contact'
+        'name': 'About',
+        'link': '#/about'
+    }, {
+        'name': 'Contact',
+        'link': '#/contact'
     }];
 }]);
 
-
-
-$('#myTab a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
+myHealth.config(function ($routeProvider) {
+    $routeProvider
+        .when('/about', {
+            templateUrl: '/app/views/about/about.html'
+        }).when('/download', {
+            templateUrl: '/app/views/download/download.html'
+        }).when('/contact', {
+            templateUrl: '/app/views/contact/contact.html'
+        }).otherwise({
+            redirectTo: '/download'
+        });
 });
 
-$(function () {
-    $('#myTab a:last').tab('show');
-})
+myHealth.directive('nav', function () {
+    return {
+        restrict: 'A',
+        controller: 'navCtrl',
+        templateUrl: '/app/views/shared/nav.html'
+    };
+});
+
+myHealth.directive('header', function () {
+    return {
+        restrict: 'A',
+        templateUrl: '/app/views/shared/header.html'
+    };
+});
+
