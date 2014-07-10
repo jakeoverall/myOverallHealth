47e52f42530ce8cd3d02be54c44d0d53c3c92ebd
joverall22 <joverall22@gmail.com>
2014-07-10 15:14:16 -0600

    adds feature add profiles

--- app/views/demo/scripts/app.js
+++ app/views/demo/scripts/app.js
@@ -1,15 +1,17 @@
-﻿var myHealthApp = angular.module('myHealthApp', ['ngRoute']);
+﻿var myHealthApp = angular.module('myHealthApp', ['ngRoute', 'ui.bootstrap']);
 
 
+//Defining Profiles
+var profiles = [];
 
-myHealthApp.directive('nav', function () {
-    return {
-        restrict: 'A',
-        controller: 'navCtrl',
-        templateUrl: '/app/views/shared/nav.html'
-    };
-});
+var Profile = function (firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.link = '#/p' + profiles.length + '_' + this.firstName.trim().split(' ').join('');
+    this.dob = dob;
+};
 
+//Controllers
 myHealthApp.controller('navCtrl', ['$scope', function ($scope) {
     $scope.nav = [{
         'name': 'Download',
@@ -20,24 +22,59 @@ myHealthApp.controller('navCtrl', ['$scope', function ($scope) {
     }];
 }]);
 
-var profiles = [];
 
-myHealthApp.controller('profileCtrl', ['$scope', function($scope) {
+myHealthApp.controller('profileCtrl', ['$scope', function ($scope) {
+    $scope.showForm = false;
     $scope.profiles = profiles;
-    $scope.profile = {
-        'firstName': '',
-        'lastName': ''
+    $scope.toggleForm = function () {
+        $scope.showForm = !$scope.showForm;
     };
-    $scope.addProfile = profiles.push(this.profile);
+    $scope.addProfile = function () {
+        var profile = new Profile(this.firstName, this.lastName);
+        profiles.push(profile);
+        document.getElementById('addProfileForm').reset();
+        $scope.showForm = false;
+        $scope.addProfileForm.$setPristine();
+        console.log(profiles);
+    };
 }]);
 
 
+myHealthApp.controller('datePickerCtrl', ['$scope', function ($scope) {
+    $scope.today = function () {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.open = function ($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.opened = true;
+    };
+
+    $scope.format = 'MMMM dd, yyyy';
+
+}]);
+
+//Directives
+myHealthApp.directive('nav', function () {
+    return {
+        restrict: 'A',
+        controller: 'navCtrl',
+        templateUrl: '/app/views/shared/nav.html'
+    };
+});
+
+
+
+
+//Routes
 myHealthApp.config(function ($routeProvider) {
     $routeProvider
         .when('/download', {
             templateUrl: '/app/views/download/download.html'
         }).when('/profiles', {
             templateUrl: '/app/views/demo/views/profiles/profiles.html',
-            controller: 'profileCtrl'
+            controller: 'profileCtrl',
         });
 });
